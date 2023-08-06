3830ea11ba474e45204bf450eb49ffe7ba4773ac
panos-geladaris <panagiotis.geladaris@transferwise.com>
2023-08-06 17:57:09 +0100

    setting up h2 and mysql

--- src/main/java/guru/springframework/sdjpaintro/bootstrap/DataInitialiser.java
+++ src/main/java/guru/springframework/sdjpaintro/bootstrap/DataInitialiser.java
@@ -3,9 +3,11 @@ package guru.springframework.sdjpaintro.bootstrap;
 import guru.springframework.sdjpaintro.domain.Book;
 import guru.springframework.sdjpaintro.repositories.BookRepository;
 import org.springframework.boot.CommandLineRunner;
+import org.springframework.context.annotation.Profile;
 import org.springframework.stereotype.Component;
 
 
+@Profile({"local", "default"})
 @Component
 public class DataInitialiser implements CommandLineRunner {
     private final BookRepository bookRepository;
@@ -16,14 +18,13 @@ public class DataInitialiser implements CommandLineRunner {
 
     @Override
     public void run(String... args) throws Exception {
+        bookRepository.deleteAll();
+
         Book bookDDD = new Book("Domain Driven Design", "123", "RandomPublisher1");
         Book bookSIA = new Book("Spring in action", "123", "RandomPublisher2");
 
-        System.out.println("id: " + bookDDD.getId());
         Book savedDDD = bookRepository.save(bookDDD);
         Book savedSIA = bookRepository.save(bookSIA);
-
-        System.out.println("id: " + savedDDD.getId());
 
         bookRepository.findAll().forEach(book -> {
             System.out.println("book id: " + book.getId());
--- src/test/java/guru/springframework/sdjpaintro/MySQLIntegrationTest.java
+++ src/test/java/guru/springframework/sdjpaintro/MySQLIntegrationTest.java
@@ -1,42 +1,27 @@
 package guru.springframework.sdjpaintro;
 
-import guru.springframework.sdjpaintro.domain.Book;
 import guru.springframework.sdjpaintro.repositories.BookRepository;
-import org.junit.jupiter.api.MethodOrderer;
-import org.junit.jupiter.api.Order;
 import org.junit.jupiter.api.Test;
-import org.junit.jupiter.api.TestMethodOrder;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.jdbc.AutoConfigureTestDatabase;
 import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
 import org.springframework.context.annotation.ComponentScan;
-import org.springframework.test.annotation.Commit;
+import org.springframework.test.context.ActiveProfiles;
 
 import static org.assertj.core.api.Assertions.assertThat;
 
-@TestMethodOrder(MethodOrderer.OrderAnnotation.class)
+@ActiveProfiles("local")
 @DataJpaTest
 @ComponentScan(basePackages = {"guru.springframework.sdjpaintro.bootstrap"})
 @AutoConfigureTestDatabase(replace = AutoConfigureTestDatabase.Replace.NONE)
-public class SpringBootJpaTestSplice {
+public class MySQLIntegrationTest {
 
     @Autowired
     BookRepository bookRepository;
 
-    @Order(1)
-    @Commit
     @Test
-    void testJpaTestSplice() {
+    void testMySQL() {
         long countBefore = bookRepository.count();
-        bookRepository.save(new Book("test book", "123455", "dummy publisher"));
-        long countAfter = bookRepository.count();
-        assertThat(countBefore).isLessThan(countAfter);
-    }
-
-    @Order(2)
-    @Test
-    void testJpaTestSpliceTransaction() {
-        long countBefore = bookRepository.count();
-        assertThat(countBefore).isEqualTo(3);
+        assertThat(countBefore).isEqualTo(2);
     }
 }
